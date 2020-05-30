5a516560c0817bf6ffd352f7f20850187e5e0c77
kharioki <antolisah@gmail.com>
2020-05-30 15:25:11 +0300

    using remark to parse markdown blog

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,25 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
 
-export default function Post() {
-  return <Layout></Layout>;
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -16,4 +33,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData
+    }
+  };
 }
